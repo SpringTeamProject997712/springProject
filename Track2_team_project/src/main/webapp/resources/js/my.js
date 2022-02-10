f7f8847fae735b1af3869e19b2e463be8f7a774e
croo12 <its19447@gmail.com>
2022-02-10 23:43:20 +0900

    나 이거 완전 실수함

--- Track2_team_project/src/main/webapp/resources/js/my.js
+++ Track2_team_project/src/main/webapp/resources/js/my.js
@@ -10,7 +10,7 @@ function go_login() {                                  //로그인
 	}
 }
 
-function go_join() {                                  //로그인
+function go_join() {                                  //회원가입
 	if(document.joinForm.id.value == "") {
 		alert("아이디를 입력하세요");
 		document.joinForm.id.focus();
