1cf6fd6e2c6f22342353a584f866ea0ad833ebd4
croo12 <its19447@gmail.com>
2022-02-11 14:47:38 +0900

    VO수정/추가

--- Track2_team_project/src/main/java/com/music/domain/AlbumVO.java
+++ Track2_team_project/src/main/java/com/music/domain/AlbumVO.java
@@ -7,33 +7,23 @@ import lombok.Data;
 @Data
 public class AlbumVO {
 	
-	private int pbno;
+	private int abno;
 	private String name;
-	private String kind;
-	private String image;
 	private String singer;
-	private String songname;
-	private String regdate;
-	private String onlines;
+	private String detail;
+	private int price;
+	private String image;
+	private Date regdate;
+	private int serial_no;
+	
 	
 //	private int pbno;
 //	private String name;
 //	private String kind;
 //	private String image;
-//	private String detail;
-//	private int price1;
-//	private int price2;
-//	private String releasedate;
-//	private int labelnum;
 //	private String singer;
 //	private String songname;
-//	private String songrealname;
 //	private String regdate;
-//	private int donate;
-//	private int viewcount;
-//	private int recommend;
-//	private int staravg;
 //	private String onlines;
-//	private int stock;
 	
 }
---
+++ Track2_team_project/src/main/java/com/music/domain/TrackVO.java
@@ -0,0 +1,17 @@
+package com.music.domain;
+
+import lombok.Data;
+
+@Data
+public class TrackVO {
+
+	private int tbno;
+	private int abno;
+	private String name;
+	private String category;
+	private String detail;
+	private int price;
+	private String songname;
+	private String songrealname;
+	private int pbno;
+}
