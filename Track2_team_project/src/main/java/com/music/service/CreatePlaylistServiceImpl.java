20def1c00c1d1a12f553b34d405f081a1a7d61eb
croo12 <its19447@gmail.com>
2022-02-15 00:34:11 +0900

    힝

--- Track2_team_project/src/main/java/com/music/service/CreatePlaylistServiceImpl.java
+++ Track2_team_project/src/main/java/com/music/service/CreatePlaylistServiceImpl.java
@@ -28,12 +28,13 @@ public class CreatePlaylistServiceImpl implements CreatePlaylistService {
 		PlaylistVO playlist = new PlaylistVO();
 		AlbumVO avo=amapper.readAlbum(track.getAbno());
 		playlist.setArtist(avo.getSinger());
-		playlist.setImage("/images/"+avo.getImage());
+		playlist.setImage(avo.getImage());
 		playlist.setMp3(track.getSongrealname());
-		playlist.setOption("<ul class='more_option'><li><a href='#'><span class='opt_icon' title='Add To Favourites'><span class='icon icon_fav'></span></span></a></li><li><a href='#'><span class='opt_icon' title='Add To Queue'><span class='icon icon_queue'></span></span></a></li><li><a href='#'><span class='opt_icon' title='Download Now'><span class='icon icon_dwn'></span></span></a></li><li><a href='#'><span class='opt_icon' title='Add To Playlist'><span class='icon icon_playlst'></span></span></a></li><li><a href='#'><span class='opt_icon' title='Share'><span class='icon icon_share'></span></span></a></li></ul>");
+		playlist.setOption("myPlayListOtion");
 		playlist.setTitle(track.getName());
 		return playlist;
-		//추후 수정할 일
+		
+		//myPlayListOtion 어케하냐
 	};
 	
 	@Override
