15a0286b5845999d3eaf432cae1acb6d9c6d3fd8
croo12 <its19447@gmail.com>
2022-02-15 11:34:54 +0900

    pull하기 전

--- Track2_team_project/src/main/java/com/music/controller/CreatePlaylistController.java
+++ Track2_team_project/src/main/java/com/music/controller/CreatePlaylistController.java
@@ -25,19 +25,23 @@ public class CreatePlaylistController {
 	
 	@GetMapping("/createPlaylist")
 	public void createPlaylistView() {
-		
 	}
 	
-	@ResponseBody
+	@ResponseBody //ajax로 플레이리스트를 받는 친구
 	@RequestMapping(value="/addRandomPlayList", produces = "application/text; charset=utf8")
 	public String addRandomPlayList() {
-		List<PlaylistVO> plist = service.addRandomPlaylist();
-		log.info(plist);
+		List<PlaylistVO> plist = addRandomPlaylist();
 		Gson gson = new Gson();
 		String json = gson.toJson(plist);
 		log.info("확ㅡ인 : "+json);
 		return json;
 	}
 	
+	private List<PlaylistVO> addRandomPlaylist(){
+		List<PlaylistVO> plist = service.addRandomPlaylist();
+		log.info("===================5개 리스트=========================");
+		log.info(plist);
+		return plist;
+	}
 	
 }
