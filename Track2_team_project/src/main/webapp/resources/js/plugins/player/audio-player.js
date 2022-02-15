0b49805fa4fa6a1e60ee937bdbfbfa78b2dbb6c1
croo12 <its19447@gmail.com>
2022-02-15 16:56:15 +0900

    주석달기

--- Track2_team_project/src/main/webapp/resources/js/plugins/player/audio-player.js
+++ Track2_team_project/src/main/webapp/resources/js/plugins/player/audio-player.js
@@ -34,7 +34,8 @@ $(function() {
     	url:"/member/loginChecker",
     	async:false,
     	success:function(data){
-    		if(data =! '1'){
+    		console.log("나온 값 : "+data);
+    		if(data != '1'){
     		login_flag=data;
     		}		
     	},error:function(xhr,status,error){
@@ -51,7 +52,6 @@ $(function() {
     //함수 가동
     if ($('.audio-player').length) {
 		var myPlayListOtion = '<ul class="more_option"><li><a href="#"><span class="opt_icon" title="Add To Favourites"><span class="icon icon_fav"></span></span></a></li><li><a href="#"><span class="opt_icon" title="Add To Queue"><span class="icon icon_queue"></span></span></a></li><li><a href="#"><span class="opt_icon" title="Download Now"><span class="icon icon_dwn"></span></span></a></li><li><a href="#"><span class="opt_icon" title="Add To Playlist"><span class="icon icon_playlst"></span></span></a></li><li><a href="#"><span class="opt_icon" title="Share"><span class="icon icon_share"></span></span></a></li></ul>';
-		console.log("뭐가 다른지 봅시다 : " + myPlayListOtion);
 		
 		$.ajax({
     	type:"get",
