23e46ba4d0b4db33852c66ac124e9de69236a8af
croo12 <its19447@gmail.com>
2022-02-17 13:23:10 +0900

    베이직 플레이리스트다?

--- Track2_team_project/src/main/java/com/music/mapper/CreatePlaylistMapper.java
+++ Track2_team_project/src/main/java/com/music/mapper/CreatePlaylistMapper.java
@@ -2,6 +2,7 @@ package com.music.mapper;
 
 import java.util.List;
 
+import com.music.domain.PlaylistVO;
 import com.music.domain.TrackVO;
 
 public interface CreatePlaylistMapper {
@@ -14,4 +15,10 @@ public interface CreatePlaylistMapper {
 	
 	//회원가입하면 베이직 플레이리스트 만들어줌(첫화면 컨트롤용)
 	public int insertBasicPlaylist(String id);
+	
+	//id로 basic_playlist의 plbno를 찾음
+	public int selectBasicPlaylistPrimaryKey(String id);
+	
+	//플레이리스트 번호로 플레이리스트와 플레이리스트 디테일 소환
+	public List<PlaylistVO> selectPlaylist(int plbno);
 }
--- Track2_team_project/src/main/java/com/music/service/CreatePlaylistServiceImpl.java
+++ Track2_team_project/src/main/java/com/music/service/CreatePlaylistServiceImpl.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.music.domain.AlbumVO;
+import com.music.domain.PlaylistVO;
 import com.music.domain.jPlayerVO;
 import com.music.domain.TrackVO;
 import com.music.mapper.AlbumMapper;
 import com.music.mapper.CreatePlaylistMapper;
+import com.music.mapper.TrackMapper;
+import com.music.security.domain.CustomUser;
 
 import lombok.Setter;
 
@@ -23,6 +28,9 @@ public class CreatePlaylistServiceImpl implements CreatePlaylistService {
 	@Setter(onMethod_ = @Autowired)
 	AlbumMapper amapper;
 	
+	@Setter(onMethod_ = @Autowired)
+	TrackMapper tmapper;
+	
 	//int를 받아서 switch문을 실행합니다. 그리고 결과로 List<PlaylistVO>를 반환합니다
 	@Override
 	public List<jPlayerVO> selectMethod(int menu) {
@@ -32,36 +40,18 @@ public class CreatePlaylistServiceImpl implements CreatePlaylistService {
 		//switch
 		switch(menu){ //받은 숫자로 스위치문 가동
 		
+		case 0: // basicPlaylist
+			
+			plist = addBasicPlaylist();
+			break;
+		
 		case 1: // 랜덤 플레이 리스트
 			plist = addRandomPlaylist();
 			break;
 		
+		
 		}//switch end
 		
-		return plist;
-	}
-	
-	//trackVO를 playlistVO로 변환하는 메서드
-	private jPlayerVO trackToPlaylist(TrackVO track) {
-		jPlayerVO playlist = new jPlayerVO();
-		AlbumVO avo=amapper.readAlbum(track.getAbno());
-		playlist.setArtist(avo.getSinger());
-		playlist.setImage("/images/"+avo.getImage());
-		playlist.setMp3(track.getSongrealname());
-		playlist.setOption("myPlayListOtion");
-		playlist.setTitle(track.getName());
-		return playlist;
-		
-		//현재 setImage 경로가 하드 코딩이라 나중에 바꿔줘야함
-	};
-	
-	private List<jPlayerVO> addRandomPlaylist() {
-		List<TrackVO> track = mapper.viewTrackList5();
-		List<jPlayerVO> plist = new ArrayList<jPlayerVO>();
-		for(int i=0; i<track.size(); i++) {
-			jPlayerVO pvo = trackToPlaylist(track.get(i));
-			plist.add(pvo);
-		}
 		return plist;
 	}
 	
@@ -70,5 +60,61 @@ public class CreatePlaylistServiceImpl implements CreatePlaylistService {
 		int result = mapper.insertBasicPlaylist(id);
 		
 		return result;
+	}
+
+//======================================= private =======================================	
+	
+	//트랙 리스트를 j플레이어리스트로 변환하는 메서드
+	private List<jPlayerVO> convertTrackToJPlyer(List<TrackVO> track) {
+		
+		List<jPlayerVO> jlist = new ArrayList<jPlayerVO>();
+		for(int i=0; i<track.size(); i++) {
+			jPlayerVO playlist = new jPlayerVO();
+			AlbumVO avo=amapper.readAlbum(track.get(i).getAbno());
+			playlist.setArtist(avo.getSinger());
+			playlist.setImage("/images/"+avo.getImage());
+			playlist.setMp3(track.get(i).getSongrealname());
+			playlist.setOption("myPlayListOtion");
+			playlist.setTitle(track.get(i).getName());
+			jlist.add(playlist);
+		}
+		return jlist;
+		//현재 setImage 경로가 하드 코딩이라 나중에 바꿔줘야함
+	};
+	
+	/*
+	 * //plbno를 받아서 특정 플레이리스트를 select private List<PlaylistVO>
+	 * selectPlaylist_view(int plbno){ return mapper.selectPlaylist(plbno); }
+	 */
+	
+	//!!! 플레이리스트를 받아서 트랙리스트로 바꿔주는 메서드
+	private List<TrackVO> convertPlaylistToTrack(List<PlaylistVO> plist){
+		List<TrackVO> tlist = new ArrayList<TrackVO>();
+		for(int i=0; i<plist.size(); i++ ) {
+			tlist.add(tmapper.selectTrack(plist.get(i).getTbno()));
+		}
+		return tlist;
+	}
+	
+	//리스트 5개 select
+	private List<jPlayerVO> addRandomPlaylist() {
+		List<jPlayerVO> plist = new ArrayList<jPlayerVO>();
+		plist = convertTrackToJPlyer(mapper.viewTrackList5());
+		return plist;
+	}
+	
+	
+	private List<jPlayerVO> addBasicPlaylist(){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String id = "";
+		if(!(auth.getPrincipal().equals("anonymousUser"))) {
+			CustomUser user = (CustomUser)auth.getPrincipal();
+			id =user.getUsername();
+		}
+		List<jPlayerVO> jlist = convertTrackToJPlyer(
+									convertPlaylistToTrack(
+										mapper.selectPlaylist(
+											mapper.selectBasicPlaylistPrimaryKey(id))));
+		return jlist;
 	}
 }
