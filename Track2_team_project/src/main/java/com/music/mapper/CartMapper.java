d46b22c9dadb1757a6671f7bf5c51724b6dca31d
EthanEric22 <ryu00209@gmail.com>
2022-02-24 11:30:38 +0900

    2-24 cart List working

--- Track2_team_project/src/main/java/com/music/mapper/CartMapper.java
+++ Track2_team_project/src/main/java/com/music/mapper/CartMapper.java
@@ -1,9 +1,12 @@
 package com.music.mapper;
 
+import java.util.List;
+
 import com.music.domain.CartVO;
 
 public interface CartMapper {
 	
 	public void insertCart(CartVO cvo);
 	
+	public List<CartVO> cartList(String id);
 }
